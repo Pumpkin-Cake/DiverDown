1697d5a8250e6a0da2e48dea3ddb86f83aaf49b7
Pumpkin-Cake <Pumpkin-Cake@users.noreply.github.com>
2026-06-08 22:57:18 +0000

    Renaming with your datas

---
+++ DiverDown/DiverDown.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace DiverDown
+{
+	public class DiverDown : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(DiverDown)} has loaded!");
+		}
+	}
+}
--- DiverDown/Loader.cs
+++ DiverDown/Loader.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace WinchModTemplate
+namespace DiverDown
 {
 	public class Loader
 	{
@@ -9,8 +9,8 @@ namespace WinchModTemplate
 		/// </summary>
 		public static void Initialize()
 		{
-			var gameObject = new GameObject(nameof(WinchModTemplate));
-			gameObject.AddComponent<WinchModTemplate>();
+			var gameObject = new GameObject(nameof(DiverDown));
+			gameObject.AddComponent<DiverDown>();
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
